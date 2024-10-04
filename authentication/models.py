82bcb580f99bfe71149798890dd0f893997cf845
One-percent-athlete <one.percent.athlete@gmail.com>
2024-10-04 14:50:40 +0900

    Create model and config settings.py

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class User(AbstractUser):
+    username = models.CharField(max_length=120)
+    email = models.EmailField(unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return f"{self.username} - Staff: {self.is_staff} - Superuser: {self.is_superuser}"
